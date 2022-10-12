2b5bdc2e881fd515687a4ae136268ac572d6fd5c
sasiofficials <115637797+sasiofficials@users.noreply.github.com>
2022-10-12 18:50:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ import pandas as pd
 # folder path
 dir_path = r'C:\Done'
 count = len(fnmatch.filter(os.listdir(dir_path), '*.*'))
+
+# prints the folders count
 print('File Count:', count)
+
+#list all the folders in the present folder
 entries = os.listdir(dir_path)
 folder_count = 0
 final_df =pd.DataFrame()
+
+# reads the particular csv files present inside the folder
 while folder_count < count:
     folder_name = entries[folder_count]
     final_df.loc[folder_count+1, 1] = folder_name
@@ -23,4 +29,5 @@ while folder_count < count:
     cleanoutputcards = len(df1.index)
     final_df.loc[folder_count + 1, 3] = cleanoutputcards
     folder_count=folder_count+1
+# write the folders and csv files row counts in a csv file
 final_df.to_csv("./count.csv",index=False,header=None)
